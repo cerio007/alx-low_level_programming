de84b5accda72df7d5e9e57527e6a797b3a26273
cerio007 <abosicyprian07@gmail.com>
2022-03-11 11:14:13 -0800

    Use of variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all numbers of base 10 starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int z;
+	for (z = 0; z < 10; z++)
+		prinf("%i", z);
+	return (0);
+}
