a06712631ee727fd7006a6638cadaed541efad98
cerio007 <abosicyprian07@gmail.com>
2022-03-11 07:34:20 -0800

    Use of variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n > 0) {
-		printf("is positive\n");
+	if ( n > 0)
+       	{
+		printf("%d is positive\n", n);
 	}
-	else if ( n == 0) {
-		printf("is zero\n");
+	else if ( n == 0)
+       	{
+		printf("%d is zero\n", n);
 	}
-	else {
-		printf("is negative\n");
+	else 
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
