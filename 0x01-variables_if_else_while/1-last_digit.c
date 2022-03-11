20f2fb055a6593e17c2376bfa9f344117fc05df3
cerio007 <abosicyprian07@gmail.com>
2022-03-11 09:10:18 -0800

    Use of variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	z = n % 10;
 	if (z > 5)
-		printf("Last digit of % is %d and is greater than 5\n", n, z);
+		printf("Last digit of %d is %d and is greater than 5\n", n, z);
 	else if (z == 0)
 		printf("Last digit of %d is %d and is 0\n", n, z);
 	else
