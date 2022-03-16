781c260acaf29c8720c294171f15cac78e50a19f
cerio007 <abosicyprian07@gmail.com>
2022-03-16 21:51:29 +0000

    Debugged error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - causes infinite loop
- * Return: Always 0
- */
+* main - Check the code
+* Return: Always 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	printf("Infinite loop incoming :(\n");
-/*
- *      i = 0;
- *
- *      while (i < 10)
- *      {
- *         putchar(i);
- *      }
- */
-	printf("Infinite loop avoided! \\o/\n");
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
