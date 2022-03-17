3ada5fa481744c9fc720cfb8fbb90eb21d2599de
cerio007 <abosicyprian07@gmail.com>
2022-03-17 07:06:05 +0000

    Debugging errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,15 @@
 
 /**
 * main - check the code
-*
 * Return: Always 0
 */
 
 int main(void)
 {
-        int n;
-
-        n = 0;
-        positive_or_negative(n);
-
+/*       int i;
+ *
+ *       i = 0;
+ *      positive_or_negative(i);
+ */
         return (0);
 }
