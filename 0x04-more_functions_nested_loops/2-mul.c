ab067fc70141ea39c09556c25793a3528dba3ba5
cerio007 <abosicyprian07@gmail.com>
2022-03-17 19:13:02 +0000

    more functions nested loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - multiplies two integers
- *
- * Return: Always 0
- */
+* main - multiplies two integers
+* Return: Always 0
+*/
+
 int mul(int a, int b)
 {
-	int i;
-	int j;
-	int k = i * j
-	{
-		_putchar("%d", k);
-	}
-	_putchar('\n');
+	int i, j, k;
+
+	printf("Multiplying two integers: \n");
+
+	scanf("%d %d", &i, &j);
+
+	k = i * j;
+
+	printf("k = %d", k);
 
 	return (0);
 }
