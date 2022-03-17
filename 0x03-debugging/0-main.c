a4597fa75c1705dffeb243f13beaa0eaa2a35149
cerio007 <abosicyprian07@gmail.com>
2022-03-17 07:12:50 +0000

    Debugging errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main - check the code
+* main - test function that prints if integer is positive or negative
 * Return: Always 0
 */
 
 int main(void)
 {
-/*       int i;
- *
- *       i = 0;
- *      positive_or_negative(i);
- */
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
         return (0);
 }
