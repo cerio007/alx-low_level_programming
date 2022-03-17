69a1adddabc67519ba159a98466daa609a0622a6
cerio007 <abosicyprian07@gmail.com>
2022-03-17 23:28:44 +0000

    more functions nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,18 @@
  * print_numbers - prints numbers between 0 to 9.
  * Return: no return
  */
+
 void print_numbers(void)
 {
-	int ch;
+	int i;
 
-	for (ch = 0; ch < 9; ch++)
+	i = o;
+
+	while (i < 10)
 	{
-		_putchar(ch);
+		_putchar(i + '0');
+
+		i++;
 	}
 	_putchar('\n');
 }
