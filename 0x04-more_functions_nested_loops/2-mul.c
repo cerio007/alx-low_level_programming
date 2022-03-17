1d73dda533ee430b6ceba440a490f942b3390937
cerio007 <abosicyprian07@gmail.com>
2022-03-17 18:31:27 +0000

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - multiplies two integers
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int i;
+	int j;
+	int k = i * j
+	{
+		_putchar("%d", k);
+	}
+	_putchar('\n');
+
+	return (0);
+}
