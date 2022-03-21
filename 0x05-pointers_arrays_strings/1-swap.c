1b376ca82712c3357bed7620a1abadc6a6057f6c
cerio007 <abosicyprian07@gmail.com>
2022-03-21 19:31:08 +0000

    Arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Always 0
+ */
+void swao_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
