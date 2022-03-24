53c1ccce83cbd1a9f5253b55b7d80e6bf7e20b20
cerio007 <abosicyprian07@gmail.com>
2022-03-24 10:09:03 +0000

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, int count2 = 0;
+	int count = 0, count2 = 0;
 
 	while (*(dest + count) != '\0')
 	{
@@ -18,7 +18,7 @@ char *_strcat(char *dest, char *src)
 	while (count2 >= 0)
 	{
 		*(dest + count) = *(src + count2);
-		if *((src + count2) == '\0')
+		if (*(src + count2) == '\0')
 			break;
 		count++;
 		count2++;
