feba688fa21012c012abe970798e704ef41be03d
cerio007 <abosicyprian07@gmail.com>
2022-03-24 21:31:57 +0000

    more pointers, arrays and strings7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,10 @@
 /**
 * cap_string - capitalizes all strings
 * @s: input string
-* Return: rerurn the pointer to dest
+* Return: return the pointer to dest
 */
 
-char *cap_string(char *s)
+char *cap_string(char *)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
