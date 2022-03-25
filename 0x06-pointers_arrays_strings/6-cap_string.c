6d0190d4d49df29d3d03a1b2714e5d6aa195624e
cerio007 <abosicyprian07@gmail.com>
2022-03-25 00:42:43 +0000

    more pointers, arrays and strings7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,10 +6,10 @@
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int s[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
@@ -18,7 +18,7 @@ char *cap_string(char *)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == sep_words[i])
+			if (*(s + count) == s[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
