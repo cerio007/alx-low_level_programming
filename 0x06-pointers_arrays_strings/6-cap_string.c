5202147edd94ae6a0d58ecef1915f4b39bfb434b
cerio007 <abosicyprian07@gmail.com>
2022-03-25 01:03:22 +0000

    more pointers, arrays and strings7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,24 @@
 
 char *cap_string(char *s)
 {
-	int i, j, f;
-	char seps[] = {' ', '\t', '\n', ',', ';',
-	     '.', '!', '?', '"', '(', ')', '{', '}'};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	f = 'A' - 'a';
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] += f;
-	for (i = 0; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		for (j = 0; j < 13; j++)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] == seps[j])
+			if (*(s + count) == sep_words[i])
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] += f;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
+		count++;
 	}
 	return (s);
 }
