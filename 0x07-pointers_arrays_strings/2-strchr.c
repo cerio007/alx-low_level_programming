639c8c6c0f31184a49e36d966e614931a7c3bbd8
cerio007 <abosicyprian07@gmail.com>
2022-03-28 21:27:10 +0000

    Pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; *(s + i) != '\0'; i++)
+	for (; *(s + i) != NULL; i++)
 		if (*(s + i) == c)
 			return (s + i);
 	if (*(s + i) == c)
 		return (s + i);
 
-	return ('\0');
+	return (NULL);
 }
