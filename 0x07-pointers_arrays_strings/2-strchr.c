8225f1c3fcc442e7d6b77772abcf23658d05cb4a
cerio007 <abosicyprian07@gmail.com>
2022-03-28 21:30:44 +0000

    Pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include <main.h>
-
+#include <stdio.h>
 /**
 * _strchr - locates a character in a string
 * @s: string
@@ -9,9 +9,9 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	for (; *(s + i) != NULL; i++)
+	for (; *(s + i) != '\0'; i++)
 		if (*(s + i) == c)
 			return (s + i);
 	if (*(s + i) == c)
