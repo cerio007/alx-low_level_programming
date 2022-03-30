e6541b69144c5c0e98a028f10056fb3bb4862887
cerio007 <abosicyprian07@gmail.com>
2022-03-30 22:35:20 +0000

    Learning Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 = '*')
+	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s2, s1 + 1));
 	return (0);
 }
