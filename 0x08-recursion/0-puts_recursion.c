e980e330dd55893748c9b946db9277b298d18274
cerio007 <abosicyprian07@gmail.com>
2022-03-30 19:40:01 +0000

    Learning Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
 
 /**
 * _puts_recursion - print a string, followed by a new line
-* @s: string
+77* @s: string
 * Return: no return
 */
 
 void _puts_recursion(char *s)
 {
-	if (*s != /'0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
