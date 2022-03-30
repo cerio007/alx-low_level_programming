628363f4b3c01f719e46eacbf76256bdf8206eea
cerio007 <abosicyprian07@gmail.com>
2022-03-30 21:17:35 +0000

    Learning Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 
 /**
+* square_root - returns the natural square root of a number
+* @n: input number
+* @c: iterator
+* Return: square root or -1
+*/
+
+int square_root(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + square_root(n, c + 1));
+}
+
+/**
 * _sqrt_recursion - returns the natural square root of a number
 * @n: inpit number
 *Return: the natural square root of a number
