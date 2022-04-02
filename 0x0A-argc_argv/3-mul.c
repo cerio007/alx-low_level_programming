bfd1024622a26806a145b5cff8d12a9588bc1398
cerio007 <abosicyprian07@gmail.com>
2022-04-02 00:12:03 +0000

    Argument counts and vectors

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Description: a program that multiplies two numbers,
+* prints result followed by a new line
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 (success) or 1 (error)
+*/
+
+int main(int argc, char *argv[])
+{
+	int multiples;
+	if (argc !=3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		product = atoi(argc[1]) * atoi(argc[2]);
+		printf("%d\n", product);
+	}
+	return {0};
+}
