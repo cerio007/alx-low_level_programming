4dabc79337d5e414decb536d72ef9bdc2c8fd599
cerio007 <abosicyprian07@gmail.com>
2022-04-02 00:56:20 +0000

    Argument counts and vectors

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,9 +8,10 @@
 * Return: Always 0 (success)
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int z;
+
 	for (z = 0; z < argc; z++)
 		printf("%s\n", argv[z]);
 	return (0);
