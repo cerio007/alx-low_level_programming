50b86f31087fddac93cf84a1dd30cc772b7775f7
cerio007 <abosicyprian07@gmail.com>
2022-04-02 00:42:20 +0000

    Argument counts and vectors

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prinys the number of argument passed into it
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
