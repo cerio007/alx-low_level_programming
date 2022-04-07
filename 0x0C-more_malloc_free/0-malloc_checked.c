4d78f208ceb5879a865ebe3e51663f7dc186e968
cerio007 <abosicyprian07@gmail.com>
2022-04-07 18:58:34 +0000

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-* malloc_checked - program that allocates memory
+* *malloc_checked - program that allocates memory
 * @b: number of bytes allocated
 * Return: return pointer to allocated memory
 * if failed, status value is equal to 98
