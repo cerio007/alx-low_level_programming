ef4b5d0487ccacd82230c989869d19fd64e133ef
cerio007 <abosicyprian07@gmail.com>
2022-04-11 23:28:51 +0000

    structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,8 +9,6 @@
  * @owner: owner of the dog
  */
 
-typedef struct dog dog_t;
-
 struct dog
 {
 	char *name;
@@ -20,6 +18,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
