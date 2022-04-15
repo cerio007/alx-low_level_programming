8524040b32b8d012bcd3cf5ca2a3a6cde5dd3efb
cerio007 <abosicyprian07@gmail.com>
2022-04-15 01:09:34 +0000

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -25,22 +25,3 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
-
-#include <stdio.h>
-#include "variadic_functions.h"
-
-/**
- * main - check the code
- * Return: Always 0.
- */
-
-int main(void)
-{
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    _putchar(sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    _putchar(sum);
-    return (0);
-}
