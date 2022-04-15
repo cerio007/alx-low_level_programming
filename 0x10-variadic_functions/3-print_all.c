7aed59ed0b4e7e24f2f506ac61d4b4534b21f780
cerio007 <abosicyprian07@gmail.com>
2022-04-15 01:42:08 +0000

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,13 +28,13 @@ void print_all(const char * const format, ...)
 		switch (format[x])
 		{
 		case 'c':
-			printf("%c", va_arg(valist, char)), z = 1;
+			printf("%c", va_arg(valist, int)), z = 1;
 			break;
 		case 'i':
 			printf("%d", va_arg(valist, int)), z = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(valist, float)), z = 1;
+			printf("%f", va_arg(valist, double)), z = 1;
 			break;
 		case 's':
 			string = va_arg(valist, char *), z = 1;
