31ac9210f3b60fd18d03e4e0d642ee4f1d7b7061
cerio007 <abosicyprian07@gmail.com>
2022-05-02 22:05:09 +0000

    elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,10 @@
 #include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcnt1.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * print_addr - prints address
